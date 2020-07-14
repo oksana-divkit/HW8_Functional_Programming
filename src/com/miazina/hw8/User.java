ff33b8e48daeed8d73192e8664510fe00cd66a46
Oksana Miazina <oksana.myazina@gmail.com>
2020-07-14 14:55:49 +0300

    finished task 1

---
+++ src/com/miazina/hw8/User.java
@@ -0,0 +1,34 @@
+package com.miazina.hw8;
+
+public class User {
+    private String name;
+    private int age;
+    private String city;
+
+    public User(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
